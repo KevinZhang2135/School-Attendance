b23f794be0de26980320b5050cd9bd87e7641caa
Kevin <kevinbczhang@gmail.com>
2023-02-21 00:25:08 -0500

    Adding Teachers GUI

---
+++ flask_react/src/components/selectionButtions.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class SelectionButton extends Component {
+    render() {
+        return (
+            <div className="col">
+                <div className="card mt-1 mb-1 me-1 p-3 float-start" style={{ width: "30%" }}>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </p>
+                    <button className="btn bg-success text-white">
+                        Select
+                    </button>
+                </div>
+
+                <div className="card mt-1 mb-1 me-1 p-3 float-start" style={{ width: "30%" }}>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </p>
+                    <button className="btn bg-success text-white">
+                        Select
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
