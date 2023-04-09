c62f70a6e9efda5941196e64e2bc51578fbf84aa
Kevin <kevinbczhang@gmail.com>
2023-04-08 21:51:32 -0400

    Building Production Test

--- flask_react/src/App.js
+++ flask_react/src/App.js
@@ -76,6 +76,7 @@ export default class App extends Component {
     };
 
     render() {
+        console.log(this.state.csv);
         return (
             <Routes>
                 <Route path="/" element={<Home />} />
