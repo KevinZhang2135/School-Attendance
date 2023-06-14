97dff807a2d28d610f45c74899f82ac4c84c1703
Kevin <kevinbczhang@gmail.com>
2023-06-13 21:29:02 -0400

    Commits

--- flask_react/backend/app.py
+++ flask_react/backend/app.py
@@ -6,15 +6,21 @@ import json
 import os
 import sys
 
-dir = os.path.dirname(sys.argv[0])
-CSV_FILE_PATH = f'{os.path.dirname(dir)}/schedules/schedule.csv'
+# csv file path
+cwd = os.path.dirname(sys.argv[0])
+CSV_FILE_PATH = f'{os.path.dirname(cwd)}/schedules/schedule.csv'
+
 api = Flask(__name__)
 CORS(api)
 
 @api.route('/', methods=['POST', 'GET'])
 def handle_fetch_request():
+    # post request
     if request.method == 'POST':
+        # extracts json data from react post
         data = request.get_json()
+
+        # rewrites csv with json data
         with open(CSV_FILE_PATH, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             for row in data.values():
@@ -28,6 +34,7 @@ def handle_fetch_request():
             for row in csv.reader(csv_file):
                 data.append(row)
 
+        # returns csv data in json format on react component mount
         json_string = json.dumps(data, indent=4)
         return json_string
 
